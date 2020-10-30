d4ff0764b5e2e9e19142cea873618c86e359fc90
walimike <walimike139@gmail.com>
2020-10-30 08:07:35 +0300

    modify class instance variables

--- sales_tax.rb
+++ sales_tax.rb
@@ -1,20 +1,21 @@
 class SalesTax
+    @@sales_tax = 0
+    @@commodities = Array.new
 
     def initiate
-        commodities = Array.new
         p 'Provide commodity and price and press Enter'
         commodity = gets.chomp
-        self.add_item(commodity, commodities)    
+        self.add_item(commodity)    
     end
 
-    def add_item(commodity, array)
+    def add_item(commodity)
         if commodity.empty?
-            process_taxes array
+            process_taxes @@commodities
         else
-            array.append(commodity)
+            @@commodities << commodity
             p 'Provide another commodity or leave blank to checkout'
             commodity = gets.chomp
-            self.add_item(commodity, array)
+            self.add_item(commodity)
         end
     end
 
@@ -22,14 +23,24 @@ class SalesTax
         if array.empty?
             p 'No taxes'
         else
-            p "You have #{array.length()}"
+            array.each do |item|
+                self.fuzzy_look_up(item)
+            end
+            p @@sales_tax
         end
     end
 
-    def add(x,y)
-        7
+    def fuzzy_look_up(item)
+        if item.include? 'chocolate' or item.include? 'headache' or item.include? 'book'
+            res = item.split('at')
+            @@sales_tax += res[1].to_i * 0.05
+        else
+            res = item.split('at')
+            @@sales_tax += res[1].to_i * 0.1
+        end
     end
 end
+
 
 instance = SalesTax.new
 instance.initiate
