bf1dd1662a35792981c0dd7bd25697f7df12ac52
walimike <walimike139@gmail.com>
2020-11-06 12:10:36 +0300

    modify tests

--- test.rb
+++ test.rb
@@ -32,49 +32,25 @@ describe SalesTax do
 
     it "can process tax payment for input1" do
         instance = create_new_sales_tax_instance input1
-        expect(instance.get_commodity.length()).to eq 3
+        expect(instance.commodities.length()).to eq 3
         instance.process_payment
-        expect(instance.get_sub_total).to eq 29.83
-        expect(instance.get_sales_tax).to eq 1.50
-        expect(instance.print_receipt).to output(
-            """
-            1 book : 12.49 
-            1 music CD: 16.49
-            1 chocolate bar: 0.85
-            Sales Taxes: 1.50
-            Total: 29.83
-        """)
+        expect(instance.get_sub_total).to eq "29.83"
+        expect(instance.get_sales_tax).to eq "1.50"
     end
 
     it "can process tax payment for input2" do
         instance = create_new_sales_tax_instance input2
-        expect(instance.get_commodity.length()).to eq 2
+        expect(instance.commodities.length()).to eq 2
         instance.process_payment
-        expect(instance.get_sub_total).to eq 29.83
-        expect(instance.get_sales_tax).to eq 1.50
-        expect(instance.print_receipt).to output(
-            """
-            1 imported box of chocolates: 10.50
-            1 imported bottle of perfume: 54.65
-            Sales Taxes: 7.65
-            Total: 65.15
-        """)
+        expect(instance.get_sub_total).to eq "65.15"
+        expect(instance.get_sales_tax).to eq "7.65"
     end
 
     it "can process tax payment for input3" do
         instance = create_new_sales_tax_instance input3
-        expect(instance.get_commodity.length()).to eq 4
+        expect(instance.commodities.length()).to eq 4
         instance.process_payment
-        expect(instance.get_sub_total).to eq 74.68
-        expect(instance.get_sales_tax).to eq 6.70
-        expect(instance.print_receipt).to output(
-            """
-            1 imported bottle of perfume: 32.19
-            1 bottle of perfume: 20.89
-            1 packet of headache pills: 9.75
-            1 imported box of chocolates: 11.85
-            Sales Taxes: 6.70
-            Total: 74.68
-        """)
+        expect(instance.get_sub_total).to eq "74.63"
+        expect(instance.get_sales_tax).to eq "6.65"
     end
 end
